cb7c6245d424f9f545f0dcebf1eb9dab216ab3dd
Digite seu nome aqui <Digite seu email aqui>
2017-01-07 16:45:23 -0200

    criando metodo definePreco

--- src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
+++ src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
@@ -2,19 +2,6 @@ package br.com.caelum.agiletickets.models;
 
 public enum TipoDeEspetaculo {
 	
-	CINEMA {
-		double porcentagemIngressosDisponiveis = 0.05;
-		
-	}, SHOW {
-		double porcentagemIngressosDisponiveis = 0.05;
-	}, TEATRO {
-		double porcentagemIngressosDisponiveis = 0.5;
-	}, BALLET {
-		double porcentagemIngressosDisponiveis = 0.5;
-	}, ORQUESTRA {
-		double porcentagemIngressosDisponiveis = 0.5;
-	};
-	
-	
+	CINEMA, SHOW, TEATRO, BALLET, ORQUESTRA;
 	
 }

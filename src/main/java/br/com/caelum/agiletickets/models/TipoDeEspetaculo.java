91e0f868de14dbbadba4487467e00eb4de6dd76f
Digite seu nome aqui <Digite seu email aqui>
2017-01-07 16:55:46 -0200

    Mexendo em TipoDeEspetaculo

--- src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
+++ src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
@@ -1,7 +1,82 @@
 package br.com.caelum.agiletickets.models;
 
+import java.math.BigDecimal;
+
 public enum TipoDeEspetaculo {
+
+	CINEMA {
+
+		@Override
+		double ingressosParaVirarLote() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		BigDecimal porcentagemIngressosDisponiveis() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		porcentagemIngressosDisponiveis = 0.05;
+		
+	}, SHOW {
+		double porcentagemIngressosDisponiveis = 0.05;
+
+		@Override
+		double ingressosParaVirarLote() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		BigDecimal porcentagemIngressosDisponiveis() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	}, TEATRO {
+		double porcentagemIngressosDisponiveis = 0.5;
+
+		@Override
+		double ingressosParaVirarLote() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		BigDecimal porcentagemIngressosDisponiveis() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	}, BALLET {
+		double porcentagemIngressosDisponiveis = 0.5;
+
+		@Override
+		double ingressosParaVirarLote() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		BigDecimal porcentagemIngressosDisponiveis() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	}, ORQUESTRA {
+		double porcentagemIngressosDisponiveis = 0.5;
+
+		@Override
+		double ingressosParaVirarLote() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		BigDecimal porcentagemIngressosDisponiveis() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	};
 	
-	CINEMA, SHOW, TEATRO, BALLET, ORQUESTRA;
-	
+	abstract double ingressosParaVirarLote();
+	abstract BigDecimal porcentagemIngressosDisponiveis();
 }
